4d2203998a93bf9c7195f68441f502ffd38bdf6e
senichek <senichek@gmail.com>
2022-12-22 14:37:51 +0100

    Initial setup

---
+++ src/main/java/com/lessonscheduler/LessonSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.lessonscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessonscheduler/LessonSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessonscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
