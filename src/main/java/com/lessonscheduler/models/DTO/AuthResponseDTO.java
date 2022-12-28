911e259f65de031c9b77c5c6120acbda67d1575e
senichek <senichek@gmail.com>
2022-12-28 10:38:46 +0100

    Login returns more details now

--- src/main/java/com/lessonscheduler/models/DTO/AuthResponseDTO.java
+++ src/main/java/com/lessonscheduler/models/DTO/AuthResponseDTO.java
@@ -5,8 +5,14 @@ import lombok.Data;
 @Data
 public class AuthResponseDTO {
     private String jwtToken = "Bearer ";
+    private Integer id;
+    private String name;
+    private String role;
 
-    public AuthResponseDTO(String accessToken) {
+    public AuthResponseDTO(Integer id, String name, String role, String accessToken) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
         this.jwtToken = accessToken;
     }
 }
